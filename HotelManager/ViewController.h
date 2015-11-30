70274b2e8e58362c72e2a869dc7d9f5dd4231db9
Lynn Kuhlman <kuhlman.lynn@gmail.com>
2015-11-30 13:01:36 -0800

    Initial Commit

---
+++ HotelManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelManager
+//
+//  Created by Regular User on 11/30/15.
+//  Copyright © 2015 Lynn Kuhlman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
